5517c502296ecd997621bb0e31bdcc1ffff7b4e7
Aashu <Soumy@threye.com>
2025-12-15 14:24:49 +0530

    Optimization
    Batch instance adding , max instances per frame
    Select CesiumVertexSampler actor in level, in details panel you'll see:
    Streaming Check Interval - How often to check for new tiles
    Max Instances Per Frame - Limit Spawns per frame

--- Source/Cesiumstart/CesiumVertexSampler.cpp
+++ Source/Cesiumstart/CesiumVertexSampler.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ae8626441d6eae92d6ac70595a4213997998dda983ba58851985e47ed7dd3cf
-size 19004
+oid sha256:597f740ec65b984134840c8ff6dc7f9fa350cd6ad401f4d2eeefa92c6a29990d
+size 18996
